49e7f37a61e8794ccd0beb50cf589937a6982b16
Panashe Chimhina <panashechimhina@gmail.com>
2022-11-19 02:22:21 +0000

    added index.js views, public and changed the img element in html to div

--- index.js
+++ index.js
@@ -1,80 +1,27 @@
-// DOM selectors
-const stars = document.getElementById('stars');
-const starsCtx = stars.getContext('2d');
-const slider = document.querySelector(".slider input");
-const output = document.querySelector("#speed");
+const express = require('express');
 
-// global variables
-let screen, starsElements, starsParams = { speed: 2, number: 300, extinction: 4 };
 
-// run stars
-setupStars();
-updateStars();
+const app = express();
+app.use('/public', express.static(process.cwd() + '/public'));
 
-// handle slider
-output.innerHTML = slider.value;
-slider.oninput = function () {
-    output.innerHTML = this.value;
-    starsParams.speed = this.value;
-};
-
-// update stars on resize to keep them centered
-window.onresize = function () {
-    setupStars();
-};
-
-// star constructor
-function Star() {
-    this.x = Math.random() * stars.width;
-    this.y = Math.random() * stars.height;
-    this.z = Math.random() * stars.width;
-
-    this.move = function () {
-        this.z -= starsParams.speed;
-        if (this.z <= 0) {
-            this.z = stars.width;
-        }
-    };
-
-    this.show = function () {
-        let x, y, rad, opacity;
-        x = (this.x - screen.c[0]) * (stars.width / this.z);
-        x = x + screen.c[0];
-        y = (this.y - screen.c[1]) * (stars.width / this.z);
-        y = y + screen.c[1];
-        rad = stars.width / this.z;
-        opacity = (rad > starsParams.extinction) ? 1.5 * (2 - rad / starsParams.extinction) : 1;
-
-        starsCtx.beginPath();
-        starsCtx.fillStyle = "rgba(255, 255, 255, " + opacity + ")";
-        starsCtx.arc(x, y, rad, 0, Math.PI * 2);
-        starsCtx.fill();
-    }
-}
-
-// setup <canvas>, create all the starts
-function setupStars() {
-    screen = {
-        w: window.innerWidth,
-        h: window.innerHeight,
-        c: [window.innerWidth * 0.5, window.innerHeight * 0.5]
-    };
-    window.cancelAnimationFrame(updateStars);
-    stars.width = screen.w;
-    stars.height = screen.h;
-    starsElements = [];
-    for (let i = 0; i < starsParams.number; i++) {
-        starsElements[i] = new Star();
-    }
-}
-
-// redraw the frame
-function updateStars() {
-    starsCtx.fillStyle = "black";
-    starsCtx.fillRect(0, 0, stars.width, stars.height);
-    starsElements.forEach(function (s) {
-        s.show();
-        s.move();
+// Index page (static HTML)
+app.route('/')
+    .get(function (req, res) {
+        res.sendFile(process.cwd() + '/views/portfolio.html');
     });
-    window.requestAnimationFrame(updateStars);
-}
+
+// 404 Not Found Middleware
+app.use(function (req, res, next) {
+    res.status(404)
+        .type('text')
+        .send('Not Found');
+});
+
+const portNum = process.env.PORT || 3005;
+
+// Start our server and tests!
+app.listen(portNum, () => {
+    console.log(`Listening on port ${portNum}`);
+});
+
+module.exports = app; // For testing
